11fb9d1b31eb0b51a3f88b6cff08211e969e0e4f
llamalord9 <gavratcliff@gmail.com>
2020-11-02 11:06:39 -0600

    Fixed header include in socket_tcp_manager.c (#1)

--- socket_tcp_manager/src/socket_tcp_manager.c
+++ socket_tcp_manager/src/socket_tcp_manager.c
@@ -1,3 +1,3 @@
-char* socket_tcp_manager_exposed() {
-    return "libsocket_tcp_manager working";
-}
+#include "socket_tcp_manager.h"
+
+char* socket_tcp_manager_exposed() { return "libsocket_tcp_manager working"; }
