3bbd4d6c808b81c8c3ae5ec9dd7ce16373230c15
Gavin Ratcliff <gavratcliff@gmail.com>
2020-11-21 10:44:55 -0600

    Removed line number returning, and added printing to the assert macro (#13)

--- utils/test/test_framework.h
+++ utils/test/test_framework.h
@@ -1,28 +1,35 @@
 #ifndef TEST_FRAMEWORK_H_
 #define TEST_FRAMEWORK_H_
 
+#include <stdio.h>
+
 #define SUCCESS 0
+#define FAIL -1
 
-#ifdef TEST_NO_LINES_
-
-#define ASSERT(expr)                                                           \
-    if (!(expr))                                                               \
-        return -1;
-
-#define ASSERT_IS_NONE(var)                                                    \
-    if (var != NULL)                                                           \
-        return -1;
-
-#else /*TEST_NO_LINES_*/
+#ifdef TEST_NO_PRINT_
 
 #define ASSERT(expr)                                                           \
     if (!(expr))                                                               \
-        return __LINE__;
+        return FAIL;
 
 #define ASSERT_IS_NONE(var)                                                    \
     if (var != NULL)                                                           \
-        return __LINE__;
+        return FAIL;
 
-#endif /*TEST_NO_LINES_*/
+#else /*TEST_NO_PRINT*/
+
+#define ASSERT(expr)                                                           \
+    if (!(expr)) {                                                             \
+        printf("Assert failed on line %d\n", __LINE__);                        \
+        return FAIL;                                                           \
+    }
+
+#define ASSERT_IS_NONE(var)                                                    \
+    if (var != NULL) {                                                         \
+        printf("Assert failed on line %d\n", __LINE__);                        \
+        return FAIL;                                                           \
+    }
+
+#endif /*TEST_NO_PRINT*/
 
 #endif /*TEST_FRAMEWORK_H_*/
