8fd1d15ee677aee01b13fac0cedd860183897e69
Gavin Ratcliff <gavratcliff@gmail.com>
2020-11-21 05:06:22 -0600

    Allowed disabling line numbers (#13)

--- utils/test/test_framework.h
+++ utils/test/test_framework.h
@@ -2,7 +2,18 @@
 #define TEST_FRAMEWORK_H_
 
 #define SUCCESS 0
-#define FAIL -1
+
+#ifdef TEST_NO_LINES_
+
+#define ASSERT(expr)                                                           \
+    if (!(expr))                                                               \
+        return -1;
+
+#define ASSERT_IS_NONE(var)                                                    \
+    if (var != NULL)                                                           \
+        return -1;
+
+#else /*TEST_NO_LINES_*/
 
 #define ASSERT(expr)                                                           \
     if (!(expr))                                                               \
@@ -11,5 +22,7 @@
 #define ASSERT_IS_NONE(var)                                                    \
     if (var != NULL)                                                           \
         return __LINE__;
+
+#endif /*TEST_NO_LINES_*/
 
 #endif /*TEST_FRAMEWORK_H_*/
