51d0a3bc8af99d3c1d1364d292721511061c47ec
garfr <gavratcliff@gmail.com>
2020-12-01 04:15:07 -0600

    Added comments to unit tests (#12)

--- utils/test/test_vector.c
+++ utils/test/test_vector.c
@@ -1,28 +1,33 @@
 #include "test_framework.h"
 #include "vector.h"
 
-typedef int (*test_func_sig)();
-
 int test_init_vector_of_size() {
     printf("Running %s\n", __func__);
     int result = SUCCESS;
 
+    // Test empty vector
     vector_t* test_vec = init_vector_of_size(1, 0);
-    ASSERT(test_vec->used == 0);
-    ASSERT(test_vec->item_size == 1);
-    ASSERT(test_vec->capacity == 0);
-    ASSERT(test_vec->_contents == NULL);
+    ASSERT_EQUAL(test_vec->used, 0);
+    ASSERT_EQUAL(test_vec->item_size, 1);
+    ASSERT_EQUAL(test_vec->capacity, 0);
+    ASSERT_IS_NULL(test_vec->_contents);
 
+    // Cleanup
     free_vector(test_vec);
+
+    // Test vector with actual contents
     test_vec = init_vector_of_size(4, 100);
-    ASSERT(test_vec->used == 0);
-    ASSERT(test_vec->item_size == 4);
-    ASSERT(test_vec->capacity == 128);
-    ASSERT(test_vec->_contents != NULL);
+    ASSERT_EQUAL(test_vec->used, 0);
+    ASSERT_EQUAL(test_vec->item_size, 4);
+    ASSERT_EQUAL(test_vec->capacity, 128);
+    ASSERT_NOT_NULL(test_vec->_contents);
 
+    // Cleanup
     free_vector(test_vec);
+
+    // Vector storing zero byte items makes not sense
     test_vec = init_vector_of_size(0, 100);
-    ASSERT(test_vec == NULL);
+    ASSERT_IS_NULL(test_vec);
 
     return result;
 }
@@ -31,15 +36,18 @@ int test_init_vector() {
     printf("Running %s\n", __func__);
     int result = SUCCESS;
 
+    // Test initialize vector
     vector_t* test_vec = init_vector(1);
-    ASSERT(test_vec->used == 0);
-    ASSERT(test_vec->item_size == 1);
-    ASSERT(test_vec->capacity == 0);
-    ASSERT(test_vec->_contents == NULL);
+    ASSERT_EQUAL(test_vec->used, 0);
+    ASSERT_EQUAL(test_vec->item_size, 1);
+    ASSERT_EQUAL(test_vec->capacity, 0);
+    ASSERT_IS_NULL(test_vec->_contents);
 
     free_vector(test_vec);
+
+    // Test invalid item_size
     test_vec = init_vector(0);
-    ASSERT(test_vec == NULL);
+    ASSERT_IS_NULL(test_vec);
 
     return result;
 }
@@ -48,34 +56,37 @@ int test_push_vector() {
     printf("Running %s\n", __func__);
     int result = SUCCESS;
 
+    // Test single byte items
     vector_t* vec = init_vector(sizeof(char));
-
     char c = 'A';
-    char* words = "BEANS";
 
+    // Test push one item
     push_vector(vec, (void*)&c);
-
     ASSERT_EQUAL(vec->used, 1);
     ASSERT_EQUAL(vec->capacity, 1);
     ASSERT_NOT_NULL(vec->_contents);
     ASSERT_EQUAL(vec->_contents[0], c);
 
-    push_vector(vec, (void*)&c);
 
+    // Test pushing another item
+    push_vector(vec, (void*)&c);
     ASSERT_EQUAL(vec->used, 2);
     ASSERT_EQUAL(vec->capacity, 2);
     ASSERT_EQUAL(vec->_contents[1], c);
-
     free_vector(vec);
+
+
+    // Test items of size greater than one
     vec = init_vector(sizeof(char*));
+    char* words = "BEANS";
 
+    // Push an item
     push_vector(vec, (void*)&words);
-
     ASSERT_EQUAL(vec->used, 1);
     ASSERT_EQUAL(vec->capacity, 1);
     ASSERT_NOT_NULL(vec->_contents);
-
     free_vector(vec);
+
     return result;
 }
 
@@ -83,19 +94,25 @@ int test_index_vector() {
     printf("Running %s\n", __func__);
     int result = SUCCESS;
 
+    // Mock vector, use stack allocated array as the vector doesn't grow
     char contents[] = {1, 0, 0, 0, 2, 0, 0, 0};
-    vector_t vec = {
-        ._contents = (void*)contents, .used = 2, .capacity = 2, .item_size = 4};
+    vector_t *vec = init_vector(4);
+    vec->_contents = contents;
+    vec->used = sizeof(contents);
+    vec->capacity = sizeof(contents);
 
-    int* ret = (int*)index_vector(&vec, 0);
+    // Test indexing first item
+    int* ret = (int*)index_vector(vec, 0);
     ASSERT_NOT_NULL(ret);
     ASSERT_EQUAL(*ret, 1);
 
-    ret = (int*)index_vector(&vec, 1);
+    // Test indexing second item
+    ret = (int*)index_vector(vec, 1);
     ASSERT_NOT_NULL(ret);
     ASSERT_EQUAL(*ret, 2);
 
-    ret = (int*)index_vector(&vec, 10);
+    // Test indexing invalid item
+    ret = (int*)index_vector(vec, 10);
     ASSERT_IS_NULL(ret);
 
     return result;
@@ -105,10 +122,10 @@ int test_vector() {
     printf("Running %s\n", __func__);
     int result = SUCCESS;
 
-    result += test_init_vector_of_size();
-    result += test_init_vector();
-    result += test_push_vector();
-    result += test_index_vector();
+    result &= test_init_vector_of_size();
+    result &= test_init_vector();
+    result &= test_push_vector();
+    result &= test_index_vector();
 
     printf((result == SUCCESS) ? "Vector test SUCCESS\n"
                                : "Vector test FAIL\n");
